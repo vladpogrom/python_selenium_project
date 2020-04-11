c6166e607cce2ae0bde5a11adff061fa9e822114
Vladislav <imanof66@gmail.com>
2020-04-11 22:21:40 +0500

    Add checks for login page and some fixes

--- pages/locators.py
+++ pages/locators.py
@@ -2,7 +2,8 @@ from selenium.webdriver.common.by import By
 
 
 class Links():
-    LINK = "http://selenium1py.pythonanywhere.com/"
+    LINK_URL = "http://selenium1py.pythonanywhere.com/"
+    LINK_LOGIN = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
--- test_main_page.py
+++ test_main_page.py
@@ -1,13 +1,16 @@
 from .pages.main_page import MainPage
 from .pages.locators import Links
+from .pages.login_page import LoginPage
 
 
 def test_guest_can_go_to_login_page(browser):
-    page = MainPage(browser, Links.LINK)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()                      # открываем страницу
-    page.go_to_login_page()          # выполняем метод страницы - переходим на страницу логина
+    page = MainPage(browser, Links.LINK_LOGIN)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()         # выполняем метод страницы - переходим на страницу логина
 
 def test_guest_should_see_login_link(browser):
-    page = MainPage(browser, Links.LINK)
+    page = MainPage(browser, Links.LINK_URL)
     page.open()
     page.should_be_login_link()
