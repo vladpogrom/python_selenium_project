ef91be0f8a71aadbd7f9164158247d71ba89d4da
Vladislav <imanof66@gmail.com>
2020-04-11 21:50:13 +0500

    Add changes to main file

--- test_main_page.py
+++ test_main_page.py
@@ -1,13 +1,13 @@
 from .pages.main_page import MainPage
+from .pages.locators import Links
+
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page = MainPage(browser, Links.LINK)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()                      # открываем страницу
     page.go_to_login_page()          # выполняем метод страницы - переходим на страницу логина
 
 def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
+    page = MainPage(browser, Links.LINK)
     page.open()
     page.should_be_login_link()
